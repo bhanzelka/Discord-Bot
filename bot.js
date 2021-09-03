89ff865cd9f85462466965e741c4daacbfbb13ef
bhanzelka <23laxstar@gmail.com>
2021-09-03 13:53:03 -0600

    Testing on why my commands aren't showing up in Guild

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`../commands/${file}`);
+	const command = require(`./commands/${file}`);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
@@ -24,7 +24,7 @@ for (const file of commandFiles) {
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const event = require(`../events/${file}`);
+	const event = require(`./events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
--- commands/miata.js
+++ commands/miata.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('miata')
-        .setDescription('Replies with a picture of a Miata.'),
-    async execute(interaction) {
-        await interaction.reply('Picture of Miata');
-    },
+data: new SlashCommandBuilder()
+    .setName('miata')
+    .setDescription('Replies with Miata picture!'),
+async execute(interaction) {
+    await interaction.reply('Miata picture!');
+},
 };
